d529e8b652f8b2f9720d1e21bb86dee971b64a62
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-05-23 15:55:46 +0900

    [Feat] 프로젝트 생성

---
+++ 2023-ML-Project/2023-ML-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2023-ML-Project
+//
+//  Created by 몽이 누나 on 2023/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
